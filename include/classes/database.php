a9255d545fec5681c03dcd360a235be164357175
nischal karki <mailnischal.karki@gmail.com>
2016-09-25 15:22:48 +0545

    logo link changed

--- include/classes/database.php
+++ include/classes/database.php
@@ -1,3 +1,6 @@
 <?php
     $connection = mysqli_connect('localhost', 'root', 'pentagon', 'fifty');
+    if ($connection === false) {
+        echo "sorry we have some connection problem";
+    }
 ?>
